6d3206672fc3213631bab5fdcd6a65c7b8fcfc55
AlexLiuTafe <49624479+AlexLiuTafe@users.noreply.github.com>
2019-11-25 11:32:19 +1100

    Initial commit

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShootScript : NetworkBehaviour
+{
+    //Public
+    public float fireInterval;
+    public float fireRate = 1f;
+    public float range = 100f;
+    public LayerMask mask;
+    //Private
+    private Transform shootPos;
+    private float fireFactor = 0f;
+    public Camera camera;//We use GameObject because we attach them in the Parent
+    // Start is called before the first frame update
+    void Start()
+    {
+
+		camera = GetComponentInChildren<Camera>(); //Camera in attached in player;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            HandleInput();
+        }
+    }
+    [Command]
+    void Cmd_PlayerShot()
+    {
+        Debug.Log("Player" + "id" + "has been shot");
+    }
+    void HandleInput()
+    {
+        fireFactor += Time.deltaTime;
+        fireInterval = 1 / fireRate;
+        if (fireFactor >= fireInterval)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Shoot();
+
+            }
+        }
+
+
+    }
+    void Shoot()
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position,camera.transform.forward,out hit,range))
+        {
+            //If Hit the player ID
+            if (hit.collider.CompareTag("Player"))
+            {
+                Cmd_PlayerShot();
+            }
+        }
+    }
+}
